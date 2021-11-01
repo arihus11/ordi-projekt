4146c946540acff2b504bd357153fa7c4e2cb287
Amalija Ramljak <amalija.ramljak@gmail.com>
2021-11-01 14:04:31 +0100

    add script for sprite change logic

--- The Return Of Laika/Assets/Scripts/LaikaMovement.cs
+++ The Return Of Laika/Assets/Scripts/LaikaMovement.cs
@@ -17,18 +17,24 @@ public class LaikaMovement : MonoBehaviour
 
     private MovementStates currentMovementState = MovementStates.UpwardIdle;
 
-    public SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = gameObject.transform.Find("Sprite").gameObject.GetComponent<SpriteRenderer>();
 
+        if (sprites.Length < 8) {
+            Debug.LogError("Not enough sprites!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         setCurrentMovementState();
+        setSprite();
     }
 
     private void setCurrentMovementState()
@@ -42,11 +48,33 @@ public class LaikaMovement : MonoBehaviour
         } else if (Input.GetKey(KeyCode.D)) {
             currentMovementState = getMovingOrIdle(MovementStates.RightMoving, MovementStates.RightIdle);
         } else {
-            currentMovementState = MovementStates.UpwardIdle;
+            currentMovementState = getIdleCurrentMovementState();
         }
     }
 
     private MovementStates getMovingOrIdle(MovementStates moving, MovementStates idle) {
         return Input.GetKey(KeyCode.Space) ? moving : idle;
+    }
+
+    private MovementStates getIdleCurrentMovementState()
+    {
+        switch (currentMovementState)
+        {
+            case MovementStates.UpwardMoving:
+                return MovementStates.UpwardIdle;
+            case MovementStates.LeftMoving:
+                return MovementStates.LeftIdle;
+            case MovementStates.RightMoving:
+                return MovementStates.RightIdle;
+            case MovementStates.DownwardMoving:
+                return MovementStates.DownwardIdle;
+            default:
+                return currentMovementState;
+        }
+    }
+
+    private void setSprite()
+    {
+        spriteRenderer.sprite = sprites[((int)currentMovementState)];
     }
 }
