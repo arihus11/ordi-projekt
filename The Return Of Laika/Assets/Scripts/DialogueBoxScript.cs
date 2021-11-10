8d25858c801cad22ac1252488168db9fdf33a97c
d_arih <ja.jakovac@gmail.com>
2021-11-10 20:05:40 +0100

    Empty dialogue box fix.

--- The Return Of Laika/Assets/Scripts/DialogueBoxScript.cs
+++ The Return Of Laika/Assets/Scripts/DialogueBoxScript.cs
@@ -21,7 +21,7 @@ public class DialogueBoxScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (i < 18)
+        if (i < 19)
         {
             if (activatedFirst == true)
             {
