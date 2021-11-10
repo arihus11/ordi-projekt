924ad860cdcc7ad2342f40ac4ecef9e3b37883de
Reninjo <ilovekroos@gmail.com>
2021-11-10 18:38:51 +0100

    Kreiran i animiran užareni planet + implementiran damage

---
+++ The Return Of Laika/Assets/Scripts/LaikaHealth.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LaikaHealth : MonoBehaviour
+{
+    public int health = 5;
+    public bool isWaiting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(health.ToString());
+        //public int healthAmount = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             
+    }
+
+    void OnTriggerStay2D(Collider2D col) 
+    {        
+        if (col.gameObject.tag == "Glow") 
+        {
+            if (isWaiting == false)
+            {
+                health--;
+                Debug.Log(health.ToString());
+                StartCoroutine(DamageForTwoSeconds());
+            }
+        }
+    }
+
+    IEnumerator DamageForTwoSeconds()
+    { 
+        isWaiting = true;
+        yield return new WaitForSecondsRealtime(2);
+        isWaiting = false;
+    }
+
+}
