9855cf358ae7977e6dc70d02e23ab7188a9de590
Amalija Ramljak <amalija.ramljak@gmail.com>
2021-11-21 13:37:22 +0100

    add collision enum helpers and ship part ID helper

---
+++ The Return Of Laika/Assets/Scripts/ShipPartID.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Laika.Utils;
+
+public class ShipPartID : MonoBehaviour
+{
+    public ShipPartEnum shipPartID;
+}
--- The Return Of Laika/Assets/Scripts/Utils.cs
+++ The Return Of Laika/Assets/Scripts/Utils.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Laika.Utils
 {
     public enum MovementState
@@ -16,6 +18,44 @@ namespace Laika.Utils
     public enum CollisionMessageObjectTag
     {
         boundary,
-        grabbable
+        grabbableItem,
+        grabbableShip,
+    }
+
+    public enum ShipPartEnum
+    {
+        None,
+        Part1,
+        Part2,
+        Part3,
+        Part4
+    }
+
+    public class Methods
+    {
+        public static bool isGrabbable(string tag)
+        {
+            return tag == CollisionMessageObjectTag.grabbableShip.ToString() || tag == CollisionMessageObjectTag.grabbableItem.ToString();
+        }
+
+        public static bool isGrabbableShip(string tag)
+        {
+            return tag == CollisionMessageObjectTag.grabbableShip.ToString();
+        }
+
+        public static bool isGrabbableItem(string tag)
+        {
+            return tag == CollisionMessageObjectTag.grabbableItem.ToString();
+        }
+
+        public static bool isBoundary(string tag)
+        {
+            return tag == CollisionMessageObjectTag.boundary.ToString();
+        }
+
+        public static ShipPartEnum getShipPartID(GameObject shipPart)
+        {
+            return shipPart.GetComponent<ShipPartID>().shipPartID;
+        }
     }
 }
