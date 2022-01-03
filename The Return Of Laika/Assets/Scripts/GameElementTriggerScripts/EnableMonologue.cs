914290b502d4e98db932f706fbf43c472fe811bb
d_arih <ja.jakovac@gmail.com>
2022-01-03 14:08:29 +0100

    Implement Push planet to grab piece puzzle
    -

--- The Return Of Laika/Assets/Scripts/GameElementTriggerScripts/EnableMonologue.cs
+++ The Return Of Laika/Assets/Scripts/GameElementTriggerScripts/EnableMonologue.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EnableMonologue : MonoBehaviour
 {
+
     public int numberOfChildToEnable;
     public float enableWalkingTime;
     public static bool destroyHoleTriggers, destroyFireballTriggers, destroyBasicPlanetTriggers;
     public Sprite emptySprite;
+    public static bool insideMonologue;
     // Start is called before the first frame update
     void Start()
     {
+        insideMonologue = false;
         destroyHoleTriggers = false;
         destroyFireballTriggers = false;
         destroyBasicPlanetTriggers = false;
@@ -26,15 +29,19 @@ public class EnableMonologue : MonoBehaviour
     {
         if (col.gameObject.tag == "Laika")
         {
-            disableMovementWhileTalking();
-            GameObject.Find("CanvasGameElementTriggers").gameObject.transform.GetChild(numberOfChildToEnable).gameObject.SetActive(true);
-            Invoke("enableMovementAfterTalking", enableWalkingTime);
+            if (insideMonologue == false)
+            {
+                disableMovementWhileTalking();
+                GameObject.Find("CanvasGameElementTriggers").gameObject.transform.GetChild(numberOfChildToEnable).gameObject.SetActive(true);
+                Invoke("enableMovementAfterTalking", enableWalkingTime);
+            }
 
         }
     }
 
     public void disableMovementWhileTalking()
     {
+        insideMonologue = true;
         GameObject.Find("JetpackSoundManager").gameObject.GetComponent<AudioSource>().enabled = false;
         GameObject.Find("Player").gameObject.GetComponent<LaikaMovement>().enabled = false;
         GameObject.FindGameObjectWithTag("laikaSprite").GetComponent<SpriteRenderer>().sprite = emptySprite;
@@ -43,6 +50,7 @@ public class EnableMonologue : MonoBehaviour
 
     public void enableMovementAfterTalking()
     {
+        insideMonologue = false;
         GameObject.Find("JetpackSoundManager").gameObject.GetComponent<AudioSource>().enabled = true;
         GameObject.Find("Player").gameObject.GetComponent<LaikaMovement>().enabled = true;
         Invoke("destoryThisBox", 0.5f);
---
+++ The Return Of Laika/Assets/Scripts/PushToGrab.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushToGrab : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "basicplanet1" || col.gameObject.tag == "basicplanet2" || col.gameObject.tag == "basicplanet3" || col.gameObject.tag == "basicplanet4" || col.gameObject.tag == "basicplanet5" || col.gameObject.tag == "basicplanet6")
+        {
+            if (this.gameObject.transform.parent.gameObject.GetComponent<CircleCollider2D>() != null)
+            {
+                try
+                {
+                    this.gameObject.transform.parent.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+                }
+                catch (MissingComponentException)
+                {
+                    Debug.Log("Ignore Exception");
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "basicplanet1" || col.gameObject.tag == "basicplanet2" || col.gameObject.tag == "basicplanet3" || col.gameObject.tag == "basicplanet4" || col.gameObject.tag == "basicplanet5" || col.gameObject.tag == "basicplanet6")
+        {
+            this.gameObject.transform.parent.gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        }
+    }
+}
