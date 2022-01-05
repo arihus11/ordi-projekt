015eb361c90ac64cd64990538c7f1a9b22a60dd7
Amalija Ramljak <amalija.ramljak@gmail.com>
2022-01-05 14:55:22 +0100

    delay death handling

--- The Return Of Laika/Assets/Scripts/LaikaHealth.cs
+++ The Return Of Laika/Assets/Scripts/LaikaHealth.cs
@@ -46,9 +46,7 @@ public class LaikaHealth : MonoBehaviour
             this.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
             GameObject.FindGameObjectWithTag("gameOverCanvas").gameObject.GetComponent<Animator>().Play("DisplayGameOverText1");
             Invoke("reverseDisplayGameOver", 4.5f);
-            // Invoke("assembleShipPartScript.handleDeath", 4.5f);
-
-            assembleShipPartScript.handleDeath();
+            assembleShipPartScript.Invoke("handleDeath", 8f);
         }
     }
 
